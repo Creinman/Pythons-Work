30e67490728ac57cfb391c20e9cae8d4b93d23e6
Creinman <68871139+Creinman@users.noreply.github.com>
2020-12-24 13:11:12 +0300

    Update task_01_307_Zhuravlev_09.py

--- task_01_307_Zhuravlev_09.py
+++ task_01_307_Zhuravlev_09.py
@@ -1,16 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from pathlib import Path
+from pathlib import Path as pth
 
-p = Path('C:/python/result')
-p.mkdir(parents=True)
-p.is_dir()
-# True
-p.is_file()
-# False
-p.rmdir()
+filename = pth('C:/python/result/task_01_307_Zhuravlev_9.txttest.txt')
+filename.touch(exist_ok=True)  
+file = open(filename, "w")
 
-f = open("C:/python/result/task_01_307_Zhuravlev_9.txttest.txt", "w")
+#file = open("C:/python/result/task_01_307_Zhuravlev_9.txttest.txt", "w")
 x = -10
 def y(x): { 
 y = 0.5 + ((np.sin(x**2))**2-0.5)/(1 + 0.001*(x**2))**2;
@@ -18,13 +14,13 @@ y1 = round(y, 2);
 return y1;
 }
 
-f.write("X    Y")
+file.write("X    Y")
 
 while x <= 10: {
-f.write("\n" + str(x) + "    " + str(y(x))); 
+file.write("\n" + str(x) + "    " + str(y(x))); 
 x = x + 0.5;
 }
-f.close()
+file.close()
 
 x = np.arange(-10, 10)
 fig, ax = plt.subplots()
